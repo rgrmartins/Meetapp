825d1d2ab6b70accda572b806382ba40b2776964
Roger Martins <rogermartinsdesign@gmail.com>
2019-09-11 12:06:15 -0400

    Conserto nome da migration e nome de tabela

--- src/database/migrations/20190907213105-create-subscriptions.js
+++ src/database/migrations/20190907213105-create-subscriptions.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('subscription', {
+    return queryInterface.createTable('subscriptions', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -36,6 +36,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('subscription');
+    return queryInterface.dropTable('subscriptions');
   },
 };
